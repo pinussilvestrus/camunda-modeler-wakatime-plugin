f06790baed63ec0b239b9668b8bbeccd6aa71d7e
Niklas Kiefer <niklas.kiefer@camunda.com>
2019-04-27 13:33:37 +0200

    chore(webpack): set library to umd mode

--- webpackMenu.config.js
+++ webpackMenu.config.js
@@ -6,7 +6,9 @@ module.exports = {
   entry: './menu/menu.js',
   output: {
     path: path.resolve(__dirname, 'menu'),
-    filename: 'menu-bundle.js'
+    filename: 'menu-bundle.js',
+    library: 'menu',
+    libraryTarget: 'umd'
   },
   target: 'electron-main'
 };
